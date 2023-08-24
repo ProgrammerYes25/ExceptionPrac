302e1f668ac56e8a4a4b510ce508568284ba9c17
yes25 <programmer.yes25@gmail.com>
2023-08-24 14:27:36 +0900

    초기 커밋.

---
+++ ExceptionPrac/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] array = { "가", "나"};
+            Console.Write(Console.ReadLine());
+            int input = int.Parse(Console.ReadLine());
+
+            if (input >= 0 && input < array.Length)
+            {
+                Console.WriteLine("입력한 위치의 값은'" + array[input] + "'입니다.");
+            }
+            else {
+                Console.WriteLine("올바른 인덱스를 입력해주세요");
+            }
+        }
+    }
+}
