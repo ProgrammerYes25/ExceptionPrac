35c381282cab2ad563d04362865259213c5c1cf4
yes25 <programmer.yes25@gmail.com>
2023-08-24 14:39:22 +0900

    커스텀 예외 객체 생성 및 활용한 예시

--- ExceptionPrac/Program.cs
+++ ExceptionPrac/Program.cs
@@ -13,15 +13,31 @@ namespace ExceptionPrac
 
             string[] array = { "가", "나"};
             Console.Write(Console.ReadLine());
-            int input = int.Parse(Console.ReadLine());
-
-            if (input >= 0 && input < array.Length)
+            try
             {
+
+                int input = int.Parse(Console.ReadLine());
                 Console.WriteLine("입력한 위치의 값은'" + array[input] + "'입니다.");
             }
-            else {
-                Console.WriteLine("올바른 인덱스를 입력해주세요");
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("인덱스 범위를 벗어났습니다.");
             }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("숫자를 입력해주세요!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("옳게 실해되는 예외가 발생하든 실행됨");
+            }
+
         }
     }
 }
